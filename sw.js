165c58f08d8c76b5baf7e4faef87ed87d1e4b704
Creativeelegantprojects <Creativeelegantprojects@gmail.com>
2026-08-20 17:34:43 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE='b-radar-draft1-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request).catch(()=>caches.match(e.request))
+  );
+});
